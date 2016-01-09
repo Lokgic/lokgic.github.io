af9c8e62998b25f34daaec78aecc01b12221d9ea
Lokgic <lok.chan@duke.edu>
2016-01-09 01:33:56 -0500

    update background

--- js/randombg.js
+++ js/randombg.js
@@ -13,15 +13,17 @@ $(function() {
 	"quote": "Logic is justly considered the basis of all other sciences, even if only for the reason that in every argument we employ concepts taken from the field of logic, and that ever correct inference proceeds in accordance with its laws."},
 	{"lname": "Russell", "fname":"Bertrand", "source":"Principle of Mathematics",
 	"quote": "The fact that all Mathematics is Symbolic Logic is one of the greatest discoveries of our age; and when this fact has been established, the remainder of the principles of mathematics consists in the analysis of Symbolic Logic itself."},
-	{"lname": "Peirce", "fname":"Charles Sander", "source": "Illustration of the Logic Science",
-	"quote": "Few persons care to study logic, because everybody conceives himself to be proficient enough in the art of reasoning already. But I observe that this satisfaction is limited to one's own ratiocination and does not extend to that of other men. We come to the full possession of our power of drawing inferences the last of all our faculties, for it is not so much a natural gift as a long and difficult art."}
+	{"lname": "Peirce", "fname":"Charles Sander", "source": "Illustration of the Logic of Science",
+	"quote": "Few persons care to study logic, because everybody conceives himself to be proficient enough in the art of reasoning already. But I observe that this satisfaction is limited to one's own ratiocination and does not extend to that of other men. We come to the full possession of our power of drawing inferences the last of all our faculties, for it is not so much a natural gift as a long and difficult art."},
+	{"lname": "Carroll", "fname": "Lewis", "source": "Through the Looking-Glass",
+	"quote": '"Contrariwise," continued Tweedledee, "if it was so, it might be; and if it were so, it would be; but as it isn\'t, it ain\'t. That\'s logic."'}
 	]
 	var $info = $('.info');
 	var $author = $('#author');
 	var $quote = $('#quote');
 	var attrbeginning = "background: linear-gradient(0deg, #333E48, transparent ), linear-gradient(180deg, #333E48, transparent), radial-gradient(circle, transparent 10%, #787C7F 40%, #333E48 80%), rgba(17,67,85, .5) url(img/"
 	var attrending = ".png) no-repeat center"
-	var seed = Math.floor(Math.random()* 6) // get a random number from 0 to 
+	var seed = Math.floor(Math.random()* 7) // get a random number f
 
 
 	$info.attr("style", attrbeginning + data[seed].lname.toLowerCase() + attrending);
