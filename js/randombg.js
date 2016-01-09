9d29e708ced5fe6e84729aa611899000579704c9
Lokgic <lok.chan@duke.edu>
2016-01-08 23:47:22 -0500

    more background for index

--- js/randombg.js
+++ js/randombg.js
@@ -7,14 +7,21 @@ $(function() {
 	"quote": "If the task of philosophy is to break the domination of words over the human mind... then my concept notation, being developed for these purposes, can be a useful instrument for philosophers... I believe the cause of logic has been advanced already by the invention of this concept notation."},
 	
 	{"lname": "Wittgenstein", "fname": "Ludwig", "source": "Tractatus Logico-Philosophicus", 
-	"quote": "The object of philosophy is the logical clarification of thoughts."}
+	"quote": "The object of philosophy is the logical clarification of thoughts."},
+
+	{"lname": "Tarski", "fname": "Alfred", "source": "Introduction to Logic and to the Methodology of Deductive Sciences",
+	"quote": "Logic is justly considered the basis of all other sciences, even if only for the reason that in every argument we employ concepts taken from the field of logic, and that ever correct inference proceeds in accordance with its laws."},
+	{"lname": "Russell", "fname":"Bertrand", "source":"Principle of Mathematics",
+	"quote": "The fact that all Mathematics is Symbolic Logic is one of the greatest discoveries of our age; and when this fact has been established, the remainder of the principles of mathematics consists in the analysis of Symbolic Logic itself."},
+	{"lname": "Peirce", "fname":"Charles Sander", "source": "Illustration of the Logic Science",
+	"quote": "The fact that all Mathematics is Symbolic Logic is one of the greatest discoveries of our age; and when this fact has been established, the remainder of the principles of mathematics consists in the analysis of Symbolic Logic itself."}
 	]
 	var $info = $('.info');
 	var $author = $('#author');
 	var $quote = $('#quote');
 	var attrbeginning = "background: linear-gradient(0deg, #333E48, transparent ), linear-gradient(180deg, #333E48, transparent), radial-gradient(circle, transparent 10%, #787C7F 40%, #333E48 80%), rgba(17,67,85, .5) url(img/"
 	var attrending = ".png) no-repeat center"
-	var seed = Math.floor(Math.random()* 3) // get a random number from 0 to 2
+	var seed = Math.floor(Math.random()* 6) // get a random number from 0 to 
 
 
 	$info.attr("style", attrbeginning + data[seed].lname.toLowerCase() + attrending);
